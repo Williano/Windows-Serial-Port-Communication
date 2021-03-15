da4300be32172d241c7dc82a840ffffb78ae3ffb
Williano <paawilly17@gmail.com>
2021-03-15 18:34:07 -0500

    Refactor

---
+++ Src/SerialPort.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+
+#define WAIT_TIME 2000
+#define MAX_DATA_LENGTH
+
+#include <Windows.h>
+#include <iostream>
+
+
+class SerialPort
+{
+
+private:
+
+	HANDLE m_handler;
+	bool m_isConnected;
+	COMSTAT m_status;
+	DWORD m_erros;
+
+public:
+
+	explicit SerialPort(const char* portName);
+	~SerialPort();
+
+	int readSerialPort(const char* buffer, unsigned int bufferSize);
+	bool writeSerialPort(const char* buffer, unsigned int bufferSize);
+	bool isConnected();
+	void closeSerial();
+};
