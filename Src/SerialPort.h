438c1afea31fad3008fd6bea5c244808446b4af3
Williano <paawilly17@gmail.com>
2021-03-16 14:09:03 -0500

    Update

--- Src/SerialPort.h
+++ Src/SerialPort.h
@@ -24,15 +24,17 @@ public:
 	~SerialPort();
 
 
-	/*
-		Sending provided buffer to serial port,
-		returns true if succeed, false if not
-	*/
+
+	// Reading bytes from serial port to buffer, returns read bytes count, or if error occurs, returns 0
 	int readSerialPort(const char* buffer, unsigned int bufferSize);
 
+
+	// Sending provided buffer to serial port, returns true if succeed, false if not.
 	bool writeSerialPort(const char* buffer, unsigned int bufferSize);
 
 
 	bool isConnected();
+
+
 	void closeSerial();
 };
