af08158192eefe109593c01def2d98d232498982
Williano <paawilly17@gmail.com>
2021-03-16 14:31:53 -0500

    Add example of sending data to com port

--- Src/SerialPortExample.cpp
+++ Src/SerialPortExample.cpp
@@ -1,15 +1,36 @@
-﻿#include "SerialPort.h"
+﻿#define DATA_LENGTH 255
+
+#include "SerialPort.h"
 #include <iostream>
 
 
-const char* portNumber = "\\\\.\\COM4";
-
 SerialPort* commPort;
 
+const char* portNumber = "\\\\.\\COM4";
+const char* data = "Test\n";
 
 int main()
 {
+
+	// Creates Serial Port instance and initialize with COM port
 	commPort = new SerialPort(portNumber);
 
 	std::cout << "Is connected: " << commPort->isConnected() << std::endl;
+
+
+	// Sends data to COM port
+	if (commPort->isConnected())
+	{
+		bool dataSent = commPort->writeSerialPort(data, DATA_LENGTH);
+
+		if (dataSent)
+		{
+			std::cout << "Data Sent Successfully" << std::endl;
+
+		}
+		else
+		{
+			std::cerr << "Data sending failed" << std::endl;
+		}
+	}
 }
