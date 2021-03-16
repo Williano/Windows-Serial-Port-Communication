b57d304523a0a44e59106706a0e754edb1eafaab
Williano <paawilly17@gmail.com>
2021-03-16 14:19:46 -0500

    Add example

---
+++ Src/SerialPortExample.cpp
@@ -0,0 +1,15 @@
+﻿#include "SerialPort.h"
+#include <iostream>
+
+
+const char* portNumber = "\\\\.\\COM2";
+
+SerialPort* commPort;
+
+
+int main()
+{
+	commPort = new SerialPort(portNumber);
+
+	std::cout << "Is connected: " << commPort->isConnected() << std::endl;
+}
